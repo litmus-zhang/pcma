d095701233fd2cf37b15c5711bb81d971937e0c0
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-06-23 11:27:52 +0100

    chore: Update dependencies and add global notification module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorators/get_user.decorator';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  async getMe(@GetUser() user: User) {
+    return user;
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  editMe(userId: string, data: any) {
+    // return user;
+    console.log(userId, data);
+  }
+}
