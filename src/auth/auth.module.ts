9eef40b891947007cd5e72eb0be3e13406c0a7df
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-08-13 10:35:41 +0100

    chore: update import path for NotificationService in auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies';
 import { JwtModule } from '@nestjs/jwt';
-import { NotificationService } from 'src/notification/notification.service';
+import { NotificationService } from '../notification/notification.service';
 
 @Module({
   imports: [JwtModule.register({})],
